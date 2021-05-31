3d6e6383e77c382cb5369c0f4a0a758be3fb8502
anuragroy17 <anuragroy17@gmail.com>
2021-05-31 21:04:31 +0530

    scheduler added

--- src/main/java/com/anuragroy/cryptonotifier/config/WebScrapper.java
+++ src/main/java/com/anuragroy/cryptonotifier/config/WebScrapper.java
@@ -3,10 +3,12 @@ package com.anuragroy.cryptonotifier.config;
 import com.gargoylesoftware.htmlunit.WebClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.logging.Level;
 
 @Configuration
+@EnableScheduling
 public class WebScrapper {
 
     @Bean
--- src/main/java/com/anuragroy/cryptonotifier/controller/CryptoApiController.java
+++ src/main/java/com/anuragroy/cryptonotifier/controller/CryptoApiController.java
@@ -1,7 +1,10 @@
 package com.anuragroy.cryptonotifier.controller;
 
 import com.anuragroy.cryptonotifier.service.CryptoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/crypto")
 public class CryptoApiController {
 
+    Logger log = LoggerFactory.getLogger(CryptoApiController.class);
+
+    // Find your Account Sid and Token at twilio.com/console
+    public static final String ACCOUNT_SID = "AC13f5a61453837badcfb0517814cc9755";
+    public static final String AUTH_TOKEN = "9ab373b01da02da048f725f0d509acd4";
 
     @Autowired
     private CryptoService cryptoService;
@@ -19,5 +27,17 @@ public class CryptoApiController {
     @GetMapping("/doge")
     public String getDogeCoinPrice() {
           return cryptoService.getDogeCoinPrice();
+    }
+
+    @Scheduled(fixedDelay = 60000, initialDelay = 5000)
+    public void sendNotification() {
+//        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+//        Message message = Message.creator(
+//                new com.twilio.type.PhoneNumber("whatsapp:+917276585354"),
+//                new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
+//                cryptoService.getDogeCoinPrice())
+//                .create();
+
+        log.debug("Notified Whatsapp");
     }
 }
